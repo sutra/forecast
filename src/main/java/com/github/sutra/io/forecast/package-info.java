d230621bfd563182c2d1e14442e0eddaf84b18f0
Sutra Zhou <zhoushuqun@gmail.com>
2013-05-19 02:05:06 +0800

    Add package-info.java.

---
+++ src/main/java/com/github/sutra/io/forecast/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <a href="http://forecast.io/">forecast.io</a>
+ * <a href="https://developer.forecast.io/docs/v2">API</a>
+ * wrapper in Java.
+ * 
+ * @author Sutra Zhou
+ */
+package com.github.sutra.io.forecast;
